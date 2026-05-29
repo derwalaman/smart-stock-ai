a1745a2f5f1333d5d2a8026e27e3ffac4ce66abd
Aman Derwal <amanderwal02@gmail.com>
2026-05-29 18:19:08 +0530

    allowed origins update

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,6 +31,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://smart-stock-ai-iota.vercel.app",
+        "https://smartstock-api-ckdo.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
